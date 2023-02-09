b3c93ece64304e6ebe437b0bbe9cde2783a4f1d9
Pavan <pn8189977@gmail.com>
2023-02-09 05:48:01 +0000

    Money-Manger

---
+++ src/components/AppointmentItem/index.js
@@ -0,0 +1,27 @@
+// Write your code here
+import './index.css'
+
+const AppointmentItem = props => {
+  const {appointmentDetails, toggleIsStarted} = props
+  const {id, title, date, isStarted} = appointmentDetails
+  const starImgUrl = isStarted
+    ? 'https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png'
+
+  const onClickStar = () => {
+    toggleIsStarted(id)
+  }
+
+  return (
+    <li className="appointment-item">
+      <div className="header-container">
+        <p className="title">{title}</p>
+        <button type="button" className="star-button" onClick={onClickStar}>
+          <img src={starImgUrl} className="star" testid="star" alt="star" />
+        </button>
+      </div>
+      <p className="date">Date : {date}</p>
+    </li>
+  )
+}
+export default AppointmentItem
